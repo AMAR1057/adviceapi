11ca0135fa3e882d08134813987b6270f5d7fcd2
amar1057 <amar1057@stu.southtexascollege.edu>
2021-11-20 23:52:14 -0600

    updates

--- src/composables/useDefinition.js
+++ src/composables/useDefinition.js
@@ -4,22 +4,22 @@ import axios from "axios";
 const definitions = ref([]);
 
 const api = axios.create({
-  baseURL: "https://api.dictionaryapi.dev/api/v2/entries/en/hello",
+  baseURL: "https://api.dictionaryapi.dev/api/v2/entries/en/",
 });
-
 const getRandomDefinition = async () => {
-  const response = await api.get();
+  const response = await api.get("hello");
   if (response.status === 200) {
-    definitions.value = [response.data.slip];
+    definitions.value = [
+      response.data[0].meanings[0].definitions[0].definition,
+    ];
   }
 };
 export const useDefinition = () => {
   getRandomDefinition();
   const search = async (searchItem) => {
-    const response = await api.get(`search/${searchItem}`);
-
+    const response = await api.get(searchItem);
     if (response.status === 200) {
-      definitions.value = response.data.slips;
+      definitions.value = response.data.slip;
     }
   };
   return { definitions, search };
